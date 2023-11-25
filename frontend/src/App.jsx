f5ae753f4692a25b247110cde56eaf2461f5b5c3
milo2112 <mjrp.ros@gmail.com>
2023-11-25 20:04:23 -0300

    adding app and main archives

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ENDPOINT } from './constants/constants'
+import GlobalContext from './context/GlobalContext'
+import NavigationBar from './components/NavigationBar'
+import NotFound from './components/NotFound'
+import Home from './views/Home'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [destinyData, setDestinyData] = useState([])
+  const [destinyToBuy, setDestinyToBuy] = useState([])
+  const [destinyTotalPrice, setDestinyTotalPrice] = useState(0)
 
+  const getData = async (url) => {
+    try {
+      const resp = await fetch(url)
+      const data = await resp.json()
+      setDestinyData(data)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+  useEffect(() => {
+    getData(ENDPOINT)
+  }, [])
+
+  const sharedGlobal = {
+    destinyData,
+    setDestinyData,
+    destinyTotalPrice,
+    setDestinyTotalPrice,
+    destinyToBuy,
+    setDestinyToBuy
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalContext.Provider value={sharedGlobal}>
+        <BrowserRouter>
+          <NavigationBar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            {/* <Route path='/shoppingcartview' element={<ShoppingCartView />} /> */}
+            {/* <Route path='/pizzadetail/:idPizza' element={<PizzaDetail />} /> */}
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   )
 }
