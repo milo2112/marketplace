0f98b285649e89c4277760a5c1f34fc3729dc181
milo2112 <mjrp.ros@gmail.com>
2023-12-02 18:20:14 -0300

    adding Admin component

---
+++ frontend/src/components/Adminform.jsx
@@ -0,0 +1,29 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function Adminform() {
+  return (
+    <Form style={{ paddingTop: '100px', paddingLeft: '850px' }} className='justify-content-center'>
+      <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
+        <Form.Label className='text-warning'>Email</Form.Label>
+        <Form.Control type="email" placeholder="Ingrese su email" />
+        <Form.Text className="text-muted">
+          
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3 w-25" controlId="formBasicPassword">
+        <Form.Label className='text-warning'>Password</Form.Label>
+        <Form.Control type="password" placeholder="Ingrese su password" />
+      </Form.Group>
+
+      <Form.Group style={{ paddingTop: '30px', paddingLeft: '100px' }}>
+        <Button variant="warning" type="submit">
+          Login
+        </Button>
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default Adminform
