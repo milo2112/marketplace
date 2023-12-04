64b019472aee04964b0427772afc9f3f5e8b4246
milo2112 <mjrp.ros@gmail.com>
2023-12-04 20:40:55 -0300

    fixing login form

--- frontend/src/components/Loginform.jsx
+++ frontend/src/components/Loginform.jsx
@@ -1,24 +1,34 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import { Link } from 'react-router-dom'
 
 function Loginform() {
+
+
   return (
-    <Form style={{ paddingTop: '100px', paddingLeft: '850px' }} className='justify-content-center'>
+    <Form style={{ paddingTop: '50px', paddingLeft: '850px' }} className='justify-content-center'>
       <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
-        <Form.Label className='text-warning'>Email</Form.Label>
+        <Form.Label className='text-warning' style={{ fontFamily: 'Barrio', fontSize: '14px' }}>Email</Form.Label>
         <Form.Control type="email" placeholder="Ingrese su email" />
         <Form.Text className="text-muted">
           
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3 w-25" controlId="formBasicPassword">
-        <Form.Label className='text-warning'>Password</Form.Label>
+      <Form.Group className="mb-3 w-25" controlId="formBasicPassword" style={{ paddingBottom: '50px' }}>
+        <Form.Label className='text-warning' style={{ fontFamily: 'Barrio', fontSize: '14px' }}>Password</Form.Label>
         <Form.Control type="password" placeholder="Ingrese su password" />
       </Form.Group>
 
+
+      <Link style={{ color: '#ffa500', paddingLeft: '20px' }} to='/registerview/'>
+        <Form.Text className="text-warning" style={{ fontFamily: 'Barrio', fontSize: '40px' }}>
+            Registrarse
+        </Form.Text>
+      </Link>
+
       <Form.Group style={{ paddingTop: '30px', paddingLeft: '100px' }}>
-        <Button variant="warning" type="submit">
+        <Button variant="warning" type="submit" style={{ fontFamily: 'Barrio', fontSize: '14px' }}>
           Login
         </Button>
       </Form.Group>
